cbd1b1635c6a2fc809b689002af0b65b2ec6a2f8
venki200221 <venki21122@gmail.com>
2024-04-23 22:45:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Test demo app");
+})
+
+app.get("/speak",(req,res)=>{
+    res.send("Say Hello World");
+})
+
+
+app.listen("3000",()=>{
+    console.log("Server running on port 3000");
+})
